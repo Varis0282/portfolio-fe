de6f3dee81e0f79d0723d7cc9ea782916ebff618
Varis Rajak <109987290+Varis0282@users.noreply.github.com>
2025-03-04 05:19:05 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   const getdata = async (req, res) => {
     try {
       dispatch(showLoading());
-      const response = await axios.get("https://varis-portfolio-be.netlify.app/api/getdata");
+      const response = await axios.get("https://portfolio-be-akdg.onrender.com/api/getdata");
       dispatch(setPortfolioData(response.data));
       dispatch(ReloadData(false));
       dispatch(hideLoading());
